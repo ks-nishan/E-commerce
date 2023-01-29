4faceff94fe52204b0a71c782a248f164b232cc9
nishanthan <nishanthan@codevus.com>
2023-01-29 17:34:14 +0530

    categories

--- src/data.js
+++ src/data.js
@@ -2,6 +2,10 @@ import smart_watches from "./images/smart_watch.png";
 import smart_watch from "./images/smart_watch2.png";
 import citizen_watch from "./images/watch3.png";
 
+import model1 from "./images/model1.jpg";
+import model2 from "./images/model.jpg";
+import model3 from "./images/model3.webp";
+
 export const sliderItems = [
   {
     id: 1,
@@ -23,5 +27,23 @@ export const sliderItems = [
     title: "New Year Sale!!",
     desc: " Come and get upto 45% discount for selected time machine..",
     bg: "fbf0f4",
+  },
+];
+
+export const categories = [
+  {
+    id: 1,
+    img: model1,
+    title: "Samsung Smart Wartch7",
+  },
+  {
+    id: 2,
+    img: model2,
+    title: "Casio Wartch 4.5",
+  },
+  {
+    id: 1,
+    img: model3,
+    title: "Citizen  Wartch",
   },
 ];
